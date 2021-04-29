04c9ae2603d75f0d6b9497199bf07ae16e9b1da5
dani10mv <danielmirallesvargas@gmail.com>
2021-04-29 19:29:28 +0200

    primercomimit servidor

---
+++ server/index.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const app = express();
+var cors = require('cors')
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "uxwhir6xmouk90qhvwsj",
+  host: "bj5arilcdpjaediqsw4g-postgresql.services.clever-cloud.com",
+  database: "bj5arilcdpjaediqsw4g",
+  password: "412Rj0rha1ciaLWoZWZA",
+  port: 5432,
+});
+
+app.use(cors())
+app.use(express.json());
+
+
+//listar habitaciones
+const listHabitacion = async (codigo) => {
+  const client = await pool.connect();
+
+  const result = await client.query({
+    text: "SELECT * FROM habitacion; ",
+  });
+
+  await client.end();
+  return result.rows;
+};
+
+//listar sensores
+const listSensoresFromHabitacion = async (codigo) => {
+
+
+  const client = await pool.connect();
+
+  const result_apertura = await client.query(
+    "Select d.codigo,d.nombre,sa.estado from sensor as s join sensores_habitacion as sh ON s.codigo = sh.sensor_codigo join dispositivo as d using (codigo) join sensor_apertura as sa using(codigo) where sh.habitacion_codigo=$1;",[codigo]
+  );
+
+  const result_movimiento = await client.query(
+    "Select d.codigo,d.nombre,sm.estado from sensor as s join sensores_habitacion as sh ON s.codigo = sh.sensor_codigo join dispositivo as d using (codigo) join sensor_movimiento as sm using(codigo) where sh.habitacion_codigo=$1;",[codigo]
+  );
+
+  await client.end();
+
+  const res={
+    sensores_apertura: result_apertura.rows,
+    sensores_movimiento: result_movimiento.rows
+  }
+
+  return res;
+};
+
+//listar actuadores
+const listActuadoresFromHabitacion = async (codigo) => {
+  const client = await pool.connect();
+
+  const result = await client.query(
+    "Select d.codigo,d.nombre,a.estado from actuador as a join actuadores_habitacion as ah ON a.codigo = ah.actuador_codigo join dispositivo as d using (codigo) where ah.habitacion_codigo=$1;",[codigo]
+  );
+
+  await client.end();
+  return result.rows;
+};
+
+//añadir habitacion
+const addHabitacion = async (habitacion) => {
+  const client = await pool.connect();
+
+  const result = await client.query(
+    "insert into habitacion values($1,$2);",[habitacion.codigo,habitacion.nombre]
+  );
+
+  await client.end();
+  return result;
+};
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/habitaciones', async (req, res) => {
+  const q = await listHabitacion();
+  console.log(q);
+  res.send(q)
+});
+
+app.get('/dispositivos/:codigo', async (req, res) => {
+  const q = await listSensoresFromHabitacion(req.params.codigo);
+  console.log(q);
+  res.send(q)
+});
+
+app.get('/actuadores/:codigo', async (req, res) => {
+  const q = await listActuadoresFromHabitacion(req.params.codigo);
+  console.log(q);
+  res.send(q)
+});
+
+app.post('/habitacion/add', async (req, res) => {
+
+  const q = await addHabitacion(req.body);
+  console.log(q);
+  res.send(q)
+});
+
+
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
